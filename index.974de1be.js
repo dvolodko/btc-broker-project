8e0918625f8b8558459e4a657798dab7afe70e1c
dvolodko <41117606+dvolodko@users.noreply.github.com>
2023-08-31 13:06:02 +0000

    Deploying to gh-pages from @ dvolodko/btc-broker-project@662ea53fbd2ec59103641d126034f7ac939e2188 🚀

---
+++ index.974de1be.js
@@ -0,0 +1,3 @@
+!function(){let t=document.querySelectorAll(".button-hamburger");t.forEach(t=>{t.addEventListener("click",()=>{let e=t.getAttribute("data-state");e&&"closed"!==e?(t.setAttribute("data-state","closed"),t.setAttribute("aria-expanded","false")):(t.setAttribute("data-state","opened"),t.setAttribute("aria-expanded","true"))})})}();//# sourceMappingURL=index.974de1be.js.map
+
+//# sourceMappingURL=index.974de1be.js.map
