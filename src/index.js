56c25d7f0684df64cd44a2b83ea8f9ac9b6e2d9b
Denys Volodko <babyleejane@gmail.com>
2023-09-15 16:27:22 +0300

    footer and footer`s menu in progress

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 // import hamburgerButton from './js/button-hamburger';
 import './js/mobile-menu';
 import './js/accordion';
+import './js/collapsible-menu';
---
+++ src/js/collapsible-menu.js
@@ -0,0 +1,21 @@
+const collapsibleMenu = document.querySelectorAll('.footer-menu');
+
+collapsibleMenu.forEach(collapsibleSubMenu => {
+  collapsibleSubMenu.addEventListener('click', e => {
+    const activeMenu = e.target.closest('.footer-menu-item');
+    if (!activeMenu) return;
+    toggleCollapsibleMenu(activeMenu);
+  });
+});
+
+function toggleCollapsibleMenu(menuToActivate) {
+  const activeMenu = menuToActivate.querySelector('.footer-menu-submenu-list');
+
+  menuToActivate.classList.toggle('active');
+
+  if (activeMenu.style.maxHeight) {
+    activeMenu.style.maxHeight = null;
+  } else {
+    activeMenu.style.maxHeight = activeMenu.scrollHeight + 'px';
+  }
+}
