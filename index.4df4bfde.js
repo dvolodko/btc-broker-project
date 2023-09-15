2e4ba38fdb506a6b2adfbd5f2c8f6a1d9c303ec2
dvolodko <41117606+dvolodko@users.noreply.github.com>
2023-09-15 13:27:56 +0000

    Deploying to gh-pages from @ dvolodko/btc-broker-project@56c25d7f0684df64cd44a2b83ea8f9ac9b6e2d9b 🚀

---
+++ index.4df4bfde.js
@@ -0,0 +1,15 @@
+// import hamburgerButton from './js/button-hamburger';
+const e=document.querySelector(".mobile-menu-overlay"),t=document.querySelector(".button-hamburger"),c=document.querySelector(".mobile-menu-container"),r=document.querySelector("body");function a(e,t){e.classList.toggle(t)}function n(){e.classList.remove("active"),c.classList.remove("active"),r.classList.remove("no-scroll"),a(t,"active"),e.removeEventListener("click",o),document.removeEventListener("keydown",i),t.setAttribute("data-state","closed"),t.setAttribute("aria-expanded","false")}function o(e){e.currentTarget===e.target&&n()}function i(e){"Escape"===e.code&&n()}t.addEventListener("click",()=>{!// function toggleMobileMenu(element) {
+//   if (element.classList.contains('active')) {
+//     closeModal();
+//     hamburgerBtn.setAttribute('data-state', 'closed');
+//     hamburgerBtn.setAttribute('aria-expanded', 'false');
+//   } else {
+//     openModal();
+//     hamburgerBtn.setAttribute('data-state', 'opened');
+//     hamburgerBtn.setAttribute('aria-expanded', 'true');
+//   }
+// }
+function(s){let l=s.getAttribute("data-state");l&&"closed"!==l?n():(e.classList.add("active"),c.classList.add("active"),r.classList.add("no-scroll"),a(t,"active"),e.addEventListener("click",o),document.addEventListener("keydown",i),t.setAttribute("data-state","opened"),t.setAttribute("aria-expanded","true"))}(t)});const s=document.querySelectorAll(".accordion");s.forEach(e=>{e.addEventListener("click",e=>{let t=e.target.closest(".accordion-panel");t&&function(e){let t=e.querySelector("button"),c=e.querySelector(".accordion-content"),r=t.getAttribute("aria-expanded");"true"===r?(t.setAttribute("aria-expanded",!1),c.setAttribute("aria-hidden",!0)):(t.setAttribute("aria-expanded",!0),c.setAttribute("aria-hidden",!1))}(t)})});const l=document.querySelectorAll(".footer-menu");l.forEach(e=>{e.addEventListener("click",e=>{let t=e.target.closest(".footer-menu-item");t&&function(e){let t=e.querySelector(".footer-menu-submenu-list");e.classList.toggle("active"),t.style.maxHeight?t.style.maxHeight=null:t.style.maxHeight=t.scrollHeight+"px"}//# sourceMappingURL=index.4df4bfde.js.map
+(t)})});
+//# sourceMappingURL=index.4df4bfde.js.map
