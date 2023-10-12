669ce5e5c65928ae2050d5f3b93b5508297d93d6
Denys Volodko <babyleejane@gmail.com>
2023-10-12 18:49:12 +0300

    form complex validation in progress

--- src/js/forms.js
+++ src/js/forms.js
@@ -53,7 +53,7 @@ nonIPNCheckbox.addEventListener("click", () => {
 const taxResidenceElement = document.querySelector(
 	'[aria-labelledby="tax-residence"]',
 );
-const citizenshipHiddenInputsWrapper = taxResidenceElement.querySelector(
+const taxResidenceHiddenInputsWrapper = taxResidenceElement.querySelector(
 	".hidden-inputs-wrapper",
 );
 const USTaxResidentInput = document.getElementById("is-US-tax-residence");
@@ -68,12 +68,12 @@ USTaxResidentInput.addEventListener("click", () => {
 	if (nonUkraineTaxResidentInput.checked) {
 		return;
 	} else if (USTaxResidentInput.checked) {
-		citizenshipHiddenInputsWrapper.style.maxHeight =
-			citizenshipHiddenInputsWrapper.scrollHeight + "px";
+		taxResidenceHiddenInputsWrapper.style.maxHeight =
+			taxResidenceHiddenInputsWrapper.scrollHeight + "px";
 		countryUkraine.removeAttribute("selected");
 		countryUS.setAttribute("selected", true);
 	} else {
-		citizenshipHiddenInputsWrapper.style.maxHeight = null;
+		taxResidenceHiddenInputsWrapper.style.maxHeight = null;
 		countryUkraine.setAttribute("selected", true);
 		countryUS.removeAttribute("selected");
 	}
@@ -83,13 +83,75 @@ nonUkraineTaxResidentInput.addEventListener("click", () => {
 	if (USTaxResidentInput.checked) {
 		return;
 	} else if (nonUkraineTaxResidentInput.checked) {
-		citizenshipHiddenInputsWrapper.style.maxHeight =
-			citizenshipHiddenInputsWrapper.scrollHeight + "px";
+		taxResidenceHiddenInputsWrapper.style.maxHeight =
+			taxResidenceHiddenInputsWrapper.scrollHeight + "px";
 		countryUkraine.removeAttribute("selected");
 		countryDummy.setAttribute("selected", true);
 	} else {
-		citizenshipHiddenInputsWrapper.style.maxHeight = null;
+		taxResidenceHiddenInputsWrapper.style.maxHeight = null;
 		countryUkraine.setAttribute("selected", true);
 		countryDummy.removeAttribute("selected");
 	}
+});
+
+// Динамічні інпути в залежності від документу, що посвідчує особу
+
+const citizenshipElement = document.querySelector(
+	'[aria-labelledby="citizenship"]',
+);
+const citizenshipRadioGroup =
+	citizenshipElement.querySelector(".form-group-radio");
+const citizenshipHiddenInputsWrapper = citizenshipElement.querySelector(
+	".hidden-inputs-wrapper",
+);
+const passportNameInput = citizenshipElement.querySelector(
+	".passport-name-wrapper",
+);
+const passportCodeInput = citizenshipElement.querySelector(
+	".passport-code-wrapper",
+);
+const passportDateExpiredInput = citizenshipElement.querySelector(
+	".passport-date-expired-wrapper",
+);
+const passportIssuedInput = document.getElementById("passport-issued");
+
+citizenshipRadioGroup.addEventListener("click", e => {
+	const checkedRadio = e.target.closest(".form-input-radio:checked");
+	if (!checkedRadio) return;
+	switch (checkedRadio.value) {
+		case "ID card":
+			passportDateExpiredInput.classList.remove("hidden");
+			passportNameInput.classList.add("hidden");
+			passportCodeInput.classList.add("hidden");
+			passportNameInput.children[1].removeAttribute("required");
+			passportCodeInput.children[1].removeAttribute("required");
+			console.log(passportCodeInput.children);
+			passportIssuedInput.setAttribute("minlength", "4");
+			passportIssuedInput.setAttribute("maxlength", "4");
+			passportIssuedInput.setAttribute("pattern", "[0-9]{4}");
+			break;
+
+		case "passport":
+			passportCodeInput.classList.remove("hidden");
+			passportNameInput.classList.add("hidden");
+			passportDateExpiredInput.classList.add("hidden");
+			break;
+
+		case "foreign passport":
+			passportCodeInput.classList.remove("hidden");
+			passportDateExpiredInput.classList.remove("hidden");
+			passportNameInput.classList.add("hidden");
+			break;
+
+		case "other":
+			passportCodeInput.classList.remove("hidden");
+			passportDateExpiredInput.classList.remove("hidden");
+			passportNameInput.classList.remove("hidden");
+			break;
+
+		default:
+			return;
+	}
+	citizenshipHiddenInputsWrapper.style.maxHeight =
+		citizenshipHiddenInputsWrapper.scrollHeight + "px";
 });
