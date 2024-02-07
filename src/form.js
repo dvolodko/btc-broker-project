6d87e7e409390c0300a22412ed9312d15cac27d7
Denys Volodko <babyleejane@gmail.com>
2024-02-07 18:30:28 +0200

    populate form data fixed

--- src/form.js
+++ src/form.js
@@ -2,6 +2,6 @@ import "./js/mobile-menu";
 import "./js/collapsible-menu";
 import "./js/header-menu";
 import "./js/header-shadow-after-scroll";
-import "./js/forms";
 import "./js/form-handler";
+import "./js/forms";
 import "./js/api-service";
--- src/js/forms.js
+++ src/js/forms.js
@@ -19,16 +19,15 @@ const PEPHiddenInputWrapper = PEPelement.querySelector(
 const hiddenLabel = PEPHiddenInputWrapper.querySelector("label");
 const hiddenInput = PEPHiddenInputWrapper.querySelector("input");
 
+if (PEPTrueElement.checked) {
+	toExpandArea(PEPHiddenInputWrapper, hiddenLabel, hiddenInput);
+}
+
 PEPelement.addEventListener("click", () => {
 	if (PEPTrueElement.checked) {
-		PEPHiddenInputWrapper.style.maxHeight =
-			PEPHiddenInputWrapper.scrollHeight + "px";
-		hiddenLabel.classList.add("required");
-		hiddenInput.setAttribute("required", true);
+		toExpandArea(PEPHiddenInputWrapper, hiddenLabel, hiddenInput);
 	} else {
-		PEPHiddenInputWrapper.style.maxHeight = null;
-		hiddenLabel.classList.remove("required");
-		hiddenInput.removeAttribute("required");
+		toCollapseArea(PEPHiddenInputWrapper, hiddenLabel, hiddenInput);
 	}
 });
 
@@ -112,6 +111,14 @@ const citizenshipElement = document.querySelector(
 );
 const citizenshipRadioGroup =
 	citizenshipElement.querySelector(".form-group-radio");
+const passportTypeIDCard = document.getElementById("is-passport-type-id-card");
+const passportTypePassport = document.getElementById(
+	"is-passport-type-passport",
+);
+const passportTypeForeignPassport = document.getElementById(
+	"is-passport-type-foreign-passport",
+);
+const passportTypeOther = document.getElementById("is-passport-type-other");
 const citizenshipHiddenInputsWrapper = citizenshipElement.querySelector(
 	".hidden-inputs-wrapper",
 );
@@ -128,69 +135,102 @@ const passportIssuedInput = document.getElementById("passportIssued");
 const passportNumberInput = document.getElementById("passportNumber");
 const passportCodeInput = document.getElementById("passportCode");
 
-citizenshipRadioGroup.addEventListener("click", e => {
-	const checkedRadio = e.target.closest(".form-input-radio:checked");
-	if (!checkedRadio) return;
-	switch (checkedRadio.value) {
-		case "ID card":
-			toShow(passportDateExpiredInput);
-			toHide(passportNameInput);
-			toHide(passportCodeInputWrapper);
-			toChangeAttributes(
-				passportNumberInput,
-				"9",
-				"9",
-				"[0-9]{9}",
-				"012345678",
-			);
-			toChangeAttributes(passportIssuedInput, "4", "4", "[0-9]{4}", "0123");
-			break;
+if (passportTypeIDCard.checked) {
+	toExpandIDCard();
+	toExpand(citizenshipHiddenInputsWrapper);
+}
 
-		case "passport":
-			toShow(passportCodeInputWrapper);
-			toHide(passportNameInput);
-			toHide(passportDateExpiredInput);
-			toChangeAttributes(passportCodeInput, "2", "2", "", "АА");
-			toChangeAttributes(passportNumberInput, "6", "6", "[0-9]{6}", "012345");
-			toChangeAttributes(
-				passportIssuedInput,
-				"2",
-				"50",
-				"",
-				"Голосіївським РУ ГМВС України в місті Києві",
-			);
-			break;
+if (passportTypePassport.checked) {
+	toExpandPassport();
+	toExpand(citizenshipHiddenInputsWrapper);
+}
 
-		case "foreign passport":
-			toShow(passportCodeInputWrapper);
-			toShow(passportDateExpiredInput);
-			toHide(passportNameInput);
-			toChangeAttributes(passportCodeInput, "2", "2", "", "АА");
-			toChangeAttributes(passportNumberInput, "6", "6", "[0-9]{6}", "012345");
-			toChangeAttributes(passportIssuedInput, "4", "4", "[0-9]{4}", "0123");
-			break;
+if (passportTypeForeignPassport.checked) {
+	toExpandForeignPassport();
+	toExpand(citizenshipHiddenInputsWrapper);
+}
 
-		case "other":
-			toShow(passportCodeInputWrapper);
-			toShow(passportDateExpiredInput);
-			toShow(passportNameInput);
-			toChangeAttributes(passportCodeInput, "2", "16", "", "AA");
-			toChangeAttributes(
-				passportNumberInput,
-				"2",
-				"16",
-				"[0-9]{2,16}",
-				"012345",
-			);
-			toChangeAttributes(passportIssuedInput, "2", "50", "", "0123");
-			break;
+if (passportTypeOther.checked) {
+	toExpandOther();
+	toExpand(citizenshipHiddenInputsWrapper);
+}
 
-		default:
-			return;
+citizenshipRadioGroup.addEventListener("click", e =>
+	dynamicInputsForPassportType(e),
+);
+
+function dynamicInputsForPassportType(e) {
+	{
+		const checkedRadio = e.target.closest(".form-input-radio:checked");
+		if (!checkedRadio) return;
+		switch (checkedRadio.value) {
+			case "ID card":
+				toExpandIDCard();
+				break;
+
+			case "passport":
+				toExpandPassport();
+				break;
+
+			case "foreign passport":
+				toExpandForeignPassport();
+				break;
+
+			case "other":
+				toExpandOther();
+				break;
+
+			default:
+				return;
+		}
+		toExpand(citizenshipHiddenInputsWrapper);
 	}
-	citizenshipHiddenInputsWrapper.style.maxHeight =
-		citizenshipHiddenInputsWrapper.scrollHeight + "px";
-});
+}
+
+function toExpand(area) {
+	area.style.maxHeight = area.scrollHeight + "px";
+}
+
+function toExpandIDCard() {
+	toShow(passportDateExpiredInput);
+	toHide(passportNameInput);
+	toHide(passportCodeInputWrapper);
+	toChangeAttributes(passportNumberInput, "9", "9", "[0-9]{9}", "012345678");
+	toChangeAttributes(passportIssuedInput, "4", "4", "[0-9]{4}", "0123");
+}
+
+function toExpandPassport() {
+	toShow(passportCodeInputWrapper);
+	toHide(passportNameInput);
+	toHide(passportDateExpiredInput);
+	toChangeAttributes(passportCodeInput, "2", "2", "", "АА");
+	toChangeAttributes(passportNumberInput, "6", "6", "[0-9]{6}", "012345");
+	toChangeAttributes(
+		passportIssuedInput,
+		"2",
+		"50",
+		"",
+		"Голосіївським РУ ГМВС України в місті Києві",
+	);
+}
+
+function toExpandForeignPassport() {
+	toShow(passportCodeInputWrapper);
+	toShow(passportDateExpiredInput);
+	toHide(passportNameInput);
+	toChangeAttributes(passportCodeInput, "2", "2", "", "АА");
+	toChangeAttributes(passportNumberInput, "6", "6", "[0-9]{6}", "012345");
+	toChangeAttributes(passportIssuedInput, "4", "4", "[0-9]{4}", "0123");
+}
+
+function toExpandOther() {
+	toShow(passportCodeInputWrapper);
+	toShow(passportDateExpiredInput);
+	toShow(passportNameInput);
+	toChangeAttributes(passportCodeInput, "2", "16", "", "AA");
+	toChangeAttributes(passportNumberInput, "2", "16", "[0-9]{2,16}", "012345");
+	toChangeAttributes(passportIssuedInput, "2", "50", "", "0123");
+}
 
 // Утилітки
 
@@ -220,4 +260,16 @@ function toChangeAttributes(
 		inputToChange.setAttribute("pattern", pattern);
 	}
 	inputToChange.setAttribute("placeholder", placeholder);
+}
+
+function toExpandArea(areaToExpand, labelToExpand, inputToExpand) {
+	areaToExpand.style.maxHeight = areaToExpand.scrollHeight + "px";
+	labelToExpand.classList.add("required");
+	inputToExpand.setAttribute("required", true);
+}
+
+function toCollapseArea(areaToCollapse, labelToCollapse, inputToCollapse) {
+	areaToCollapse.style.maxHeight = null;
+	labelToCollapse.classList.remove("required");
+	inputToCollapse.removeAttribute("required");
 }
