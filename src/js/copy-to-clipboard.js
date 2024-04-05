f0586600aef5e875bc114b26efb898671e875c54
Denys Volodko <babyleejane@gmail.com>
2024-04-05 12:59:28 +0300

    modify copy to clipboard

--- src/js/copy-to-clipboard.js
+++ src/js/copy-to-clipboard.js
@@ -3,6 +3,6 @@ const elementToCopy = document.querySelector(".banking-details-list");
 elementToCopy.addEventListener("click", e => {
 	if (e.target.className === "banking-details-item-strong-text") {
 		console.log(e.target.innerText);
-		navigator.clipboard.writeText(e.target.className);
+		navigator.clipboard.writeText(e.target.innerText);
 	}
 });
