8970914979548703d04f7f60c313b003a0c42855
Denys Volodko <babyleejane@gmail.com>
2024-04-05 17:28:05 +0300

    add about BTC Broker page and small fixes

--- src/js/copy-to-clipboard.js
+++ src/js/copy-to-clipboard.js
@@ -2,7 +2,6 @@ const elementToCopy = document.querySelector(".banking-details-list");
 
 elementToCopy.addEventListener("click", e => {
 	if (e.target.className === "banking-details-item-strong-text") {
-		console.log(e.target.innerText);
 		navigator.clipboard.writeText(e.target.innerText);
 	}
 });
