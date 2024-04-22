eaa12531090f949e1cb4988cb113c8dee5f20eca
Denys Volodko <babyleejane@gmail.com>
2024-04-22 18:26:31 +0300

    axios request base

--- src/js/fetch-handler.js
+++ src/js/fetch-handler.js
@@ -1,7 +1,31 @@
-async function fetchBonds() {
-	const response = await fetch("https://bond.ua/landing/asset/?asset_type=0");
-	const bonds = await response.json();
-	console.log(bonds.results);
+const axios = require("axios").default;
+const BASE_URL = "https://bond.ua/landing/asset/";
+const BONDS_URL = "?asset_type=0";
+const EUROBONDS = "?asset_type=2";
+const CORPORATE = "?asset_type=3";
+
+async function getBondsQuotes(assetType) {
+	const response = await axios.get(`${BASE_URL}${assetType}`);
+	console.log(response.data.results);
+	return response.data.results;
 }
 
-fetchBonds();
+getBondsQuotes(BONDS_URL);
+
+// const axios = require("axios").default;
+// const KEY = "8d41be39d2696eeff12b89d916a6c984";
+// const api_key = `?api_key=${KEY}`;
+// const BASE_URL = "https://api.themoviedb.org/3/";
+
+// export async function getTrailersByMovieId(movieID) {
+// 	const response = await axios.get(
+// 		`${BASE_URL}movie/${movieID}/videos${api_key}`,
+// 	);
+// 	console.log(response.data);
+// 	const result = response.data.results.filter(
+// 		value => value.type === "Trailer" && value.site === "YouTube",
+// 	);
+// 	return result;
+// }
+
+// getTrailersByMovieId("693134");
